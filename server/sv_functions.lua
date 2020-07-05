8a428b4702606aeecda213f85d1a60eeeba5d0e5
Something-Debug <peyton.bowe72@gmail.com>
2020-07-05 02:06:43 -0500

    Removed weird error logging, updated logging function

--- server/sv_functions.lua
+++ server/sv_functions.lua
@@ -2,21 +2,6 @@
 local webhook = GetConvar('webhooklink', '')
 --Contact link
 local contactlink = GetConvar('contact', '')
---Error Logging
-function ValkyrieError(message)
-  local source = source
-  local embed = {
-    {
-      ['color'] = 15007744,
-      ['title'] = 'Valkyrie Error',
-      ['description'] = message,
-      ['footer'] = {
-        ['text'] = 'Valkyrie Anticheat',
-      },
-    }
-  }
-  PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = name, embeds = embed}), { ['Content-Type'] = 'application/json' })
-end
 --Identifiers
 function ValkyrieIdentifiers(player)
   for k, v in ipairs(GetPlayerIdentifiers(player)) do
@@ -25,25 +10,25 @@ function ValkyrieIdentifiers(player)
     elseif string.sub(v, 1, string.len('discord:')) == 'discord:' then
       discord = v
     elseif discord == nil or discord == '' then
-      discord = 'Discord identifier not found.'
+      discord = 'Discord identifier not found'
     elseif string.sub(v, 1, string.len('steam:')) == 'steam:' then
       steam = v
     elseif steam == nil or steam == '' then
-      steam = 'Steam identifier not found.'
+      steam = 'Steam identifier not found'
     end
   end
 end
---Kick logging
-function ValkyrieLog(message)
-  local source = source
+--logging function
+function ValkyrieLog(title, message)
   local embed = {
     {
-      ['color'] = 1,
-      ['title'] = 'Valkyrie',
+      ['title'] = 'Valkyrie: ' ..title.. '',
+      ['type'] = 'rich',
       ['description'] = message,
-      ['footer'] = {
-        ['text'] = 'Valkyrie Anticheat',
-      },
+      ['color'] = 732633,
+      ['thumbnail'] = {['url'] = 'https://i.imgur.com/jmYn66H.png'},
+      ['author'] = {['name'] = 'Valkyrie Anticheat', ['icon_url'] = 'https://i.imgur.com/jmYn66H.png'},
+      ['footer'] = {['text'] = os.date("%x (%X %p)")},
     }
   }
   PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = name, embeds = embed}), { ['Content-Type'] = 'application/json' })
@@ -51,12 +36,10 @@ end
 --Kicking function
 function ValkyrieKickPlayer(player, reason)
   if player == nil then
-    return
-    ValkyrieError('No source was set for kicking function this is a fatal error, players will not be kicked!')
+    return 'No source was set for kicking function this is a fatal error, players will not be kicked!'
   end
   if reason == nil or reason == '' then
-    return
-    ValkyrieError('No reason was set for kicking function this is a fatal error, players will not be kicked!')
+    return 'No reason was set for kicking function this is a fatal error, players will not be kicked!'
   end
   DropPlayer(player, 'Kicked \n You have been kicked for the following reason: ' ..reason..'. \n If you think this was a mistake contact us at ' ..contactlink.. '.')
 end
