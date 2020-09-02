fe5599bf5b0a8c45fa149011914c86b1c217c6a2
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2020-09-02 06:52:12 -0500

    Update deferrals.lua

--- server/deferrals.lua
+++ server/deferrals.lua
@@ -1,57 +1,29 @@
---Thanks Puntherline for the whitelist portion of this code. 
-Config = {}
-Config.Whitelist = false                    --Want to use whitelist?
-Config.Users = {                            --Any identifier will work but I recommend license
+local Whitelist = false
+local Whitelisted = {
+  ''
 }
 
-AddEventHandler('playerConnecting', function(name, setKickReason, deferrals)
+AddEventHandler('playerConnecting', function(playerName, setKickReason, deferrals)
+  local license = ValkyrieIdentifiers(source).license
 
-  local player = source
-  local identifiers = GetPlayerIdentifiers(source)
-  local licenseIdentifier
-  local allowed = false
-  local newInfo = ""
-  local oldInfo = ""
-  --Prevent connection right away
   deferrals.defer()
-  deferrals.update(string.format('Hello %s please wait while your license ID is checked', name))
-  
-  --Mandatory wait
+  deferrals.update('Hello ' ..playerName.. ' please wait while your identifiers are checked')
+
   Wait(0)
 
-  if Config.Whitelist then
-    for k1, v in pairs(identifiers) do
-        for k2, i in ipairs(Config.Users) do
-            if string.match(v, i) then
-                allowed = true
-                break
-            end
-        end
-    end
-  
-    if allowed then
-        deferrals.done()
-    else
-        for k1, v in pairs(identifiers) do
-            oldInfo = newInfo
-            newInfo = string.format("%s\n%s", oldInfo, v)
-        end
-        deferrals.done('You\'re not whitelisted join our discord for more information discord.gg/yeet')
-    end
+  if not license then
+    return deferrals.done('Unable to find license identifier, is sv_lan set?')
   end
-
-  if not Config.Whitelist then 
-    for k1, v in pairs(identifiers) do
-      if string.find(v, 'license') then 
-        licenseIdentifier = v
-        break
+  
+  if Whitelist then
+    for _, id in pairs(Whitelisted) do
+      if string.match(license, id) then
+        deferrals.done()
+      else
+        deferrals.done('You\'re not whitelisted')
       end
     end
-
-    if not licenseIdentifier then
-      deferrals.done('You\'re not connected to the rockstar services this is indicative of a fatal error.')
-    else
-      deferrals.done()
-    end
+  else 
+    deferrals.done()
   end
 end)
