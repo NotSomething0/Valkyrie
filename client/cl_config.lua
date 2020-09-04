d7468e79b2290f4dbccd05825c257d9ead20e96b
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2020-09-03 19:17:59 -0500

    Update cl_config.lua

--- client/cl_config.lua
+++ client/cl_config.lua
@@ -1,4 +1,19 @@
 Config = {}
 
-Config.GodModeCheck = true
-Config.SpectatorCheck = true
+Config.SpectatorCheck = false
+Config.MaxSpectatorStrikes = 5
+Config.GodModeCheck = false
+Config.MaxGodModeStrikes = 5
+Config.BlockedDamageTypes = { 4, 5, 8, 11}
+Config.BlockedClientEvents = {
+    "ambulancier:selfRespawn",
+    "bank:transfer",
+    "esx_ambulancejob:revive",
+    "esx-qalle-jail:openJailMenu",
+    "esx_jailer:wysylandoo",
+    "esx_society:openBossMenu",
+    "esx:spawnVehicle",
+    "esx_status:set",
+    "HCheat:TempDisableDetection",
+    "UnJP"
+}
