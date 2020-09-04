5f2fdadeac37900b1b67db01fd0d336842918880
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2020-09-03 19:24:44 -0500

    Update sv_loops.lua

--- server/sv_loops.lua
+++ server/sv_loops.lua
@@ -4,3 +4,16 @@ CreateThread(function()
         Wait(60000) --Check every minute
     end
 end)
+
+CreateThread(function()
+    while true do
+        Wait(1000)
+        local player = GetPlayerFromIdenx(0)
+        local license = ValkyrieIdentifiers(player).license
+        local playerPed = GetPlayerPed(player)
+        if GetEntityMaxHealth(playerPed) >= 205 then
+            ValkyrieLog('Player Kicked', '**Player:** ' ..GetPlayerName(player).. '\n**Reason:** Tried to set their max ped health greater then 205. \n**license:** ' ..license)
+            ValkyrieKickPlayer(player, 'Max health: ' ..GetEntityMaxHealth(playerPed))
+        end
+    end
+end)
