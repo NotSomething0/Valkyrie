34ba246074624d6cde6ec9cb8fe1a134f19321f8
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2020-09-03 19:21:27 -0500

    Update cl_loops.lua

--- client/cl_loops.lua
+++ client/cl_loops.lua
@@ -2,49 +2,34 @@ RegisterNetEvent('checkAce')
 AddEventHandler('checkAce', function(state)
     isAdmin = state
 end)
-gmStrikes = 0
-AddEventHandler('playerSpawned', function()
-    CreateThread(function()
-        while true do
-            Wait(0)
-            if Config.GodModeCheck then
-                Wait(1000)
-                if GetPlayerInvincible(PlayerId()) then
-                    TriggerServerEvent('Valkyrie:ClientDetection', GetPlayerName(PlayerId()), 'GodMode: SetPlayerInvincible()', gmStrikes, 'Invincible')
-                end
-                local playerPed = PlayerPedId()
-                local currentHealth = GetEntityHealth(playerPed)
-                SetEntityHealth(playerPed, currentHealth - 2)
-                Wait(50)
-                if not IsPlayerDead(PlayerId()) then
-                    if GetEntityHealth(playerPed) == currentHealth and GetEntityHealth(playerPed) ~= 0 then
-                        TriggerServerEvent('Valkyrie:ClientDetection', GetPlayerName(PlayerId()), 'GodMode: SetEntityHealth()', gmStrikes, 'Invincible')
-                    elseif GetEntityHealth(playerPed) == currentHealth - 2 then
-                        SetEntityHealth(playerPed, GetEntityHealth(playerPed) + 2)
-                    end
-                end
-            end
-        end
-    end)
-end)
 
-spawned = false
-strikes = 0
+local spawned = false
 AddEventHandler('playerSpawned', function()
-    if spawned == false then
+    if not spawned then
         spawned = true
+
+        local playerPed = PlayerPedId()
+        local specStrikes = 0
+        local godModeStrikes = 0
+
         CreateThread(function()
-            while true do
+            while spawned do
                 Wait(1000)
                 if Config.SpectatorCheck then
-                    Wait(500)
-                    if isAdmin then
+                    if not isAdmin then
                         if NetworkIsInSpectatorMode() then
-                            TriggerServerEvent('Valkyrie:ClientDetection', GetPlayerName(PlayerId()), 'Spectating: NetworkSetInSpectatorMode()', 'Spectating')
+                            specStrikes = specStrikes + 1
+                            print('[Valkyrie] ' ..specStrikes.. ' NetworkIsInSpectatorMode()')
                         end
+
                         local coords = #(GetEntityCoords(PlayerPedId()) - GetFinalRenderedCamCoord())
+
                         if coords >= 30 and not IsPedInFlyingVehicle(PlayerPedId()) then
-                            TriggerServerEvent('Valkyrie:ClientDetection', GetPlayerName(PlayerId()), 'Spectating: GetFinalRenderedCamCoord(): `' ..coords.. '` units.', specStrikes, 'Spectating')
+                            specStrikes = specStrikes + 1
+                            print('[Valkyrie] ' ..specStrikes.. ' vector math GetEntityCoords() - GetFinalRenderedCamCoord()')
+                        end
+                        if specStrikes >= Config.MaxSpectatorStrikes then
+                            TriggerServerEvent('Valkyrie:ClientDetection', GetPlayerName(PlayerId()), 'Spectating without permission', 'Spectating')
                         end
                     end
                 end
@@ -53,15 +38,43 @@ AddEventHandler('playerSpawned', function()
 
         CreateThread(function()
             while true do
-                Wait(0)
-                if Config.SpeedDetection then
-                    Wait(1000)
-                    if GetEntitySpeed(PlayerPedId()) >= Config.MaxSpeed then
-                        strikes = strikes + 1
+                Wait(1000)
+                if Config.GodModeCheck then
+                    if GetPlayerInvincible(PlayerId()) then
+                        TriggerServerEvent('Valkyrie:ClientDetection', GetPlayerName(PlayerId()), 'GodMode: SetPlayerInvincible()', 'Invincible')
                     end
-                    if strikes >= 3 then 
-                        TriggerServerEvent('Valkyrie:ClientDetection', GetPlayerName(PlayerId()), 'Speed: SetEntityMaxSpeed(): `' ..GetEntitySpeed(PlayerPedId()), 'Speed')
+
+                    local currentHealth = GetEntityHealth(playerPed)
+                    
+                    SetEntityHealth(playerPed, currentHealth - 2)
+                    Wait(50)
+                    if not IsPlayerDead(PlayerId()) then
+                        if GetEntityHealth(playerPed) == currentHealth and GetEntityHealth(playerPed) ~= 0 then
+                            godModeStrikes = godModeStrikes + 1
+                        end
+                        if godModeStrikes >= Config.MaxGodModeStrikes then
+                            TriggerServerEvent('Valkyrie:ClientDetection', GetPlayerName(PlayerId()), 'GodMode: SetEntityHealth()', 'Invincible')
+                        elseif GetEntityHealth(playerPed) == currentHealth - 2 then
+                            SetEntityHealth(playerPed, GetEntityHealth(playerPed) + 2)
+                        end
                     end
+                end
+            end
+        end)
+
+        CreateThread(function()
+            while spawned do
+                Wait(1000)
+                local wep = GetSelectedPedWeapon(playerPed)
+                for _, dmgType in pairs(Config.BlockedDamageType) do
+                    if GetWeaponDamageType(wep) == dmgType then
+                        RemoveWeaponFromPed(playerPed, wep)
+                        print('[Valkyrie] Weapon removed from player ' ..GetEntityModel(wep).. ' blocked damage type.')
+                    end
+                end
+                if GetWeaponDamageModifier(wep) >= 10 then
+                    RemoveWeaponFromPed(playerPed, wep)
+                    print('[Valkyrie] Weapon removed from player ' ..GetEntityModel(wep).. ' exceeded maximum damage modifier.')
                 end
             end
         end)
