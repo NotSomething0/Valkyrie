03efd5382287752237076f411572f694fccc5db9
Something-Debug <peyton.bowe72@gmail.com>
2020-09-11 20:44:03 -0500

    Remove event list

--- client/cl_config.lua
+++ client/cl_config.lua
@@ -5,15 +5,3 @@ Config.MaxSpectatorStrikes = 5
 Config.GodModeCheck = false
 Config.MaxGodModeStrikes = 5
 Config.BlockedDamageTypes = { 4, 5, 8, 11}
-Config.BlockedClientEvents = {
-    "ambulancier:selfRespawn",
-    "bank:transfer",
-    "esx_ambulancejob:revive",
-    "esx-qalle-jail:openJailMenu",
-    "esx_jailer:wysylandoo",
-    "esx_society:openBossMenu",
-    "esx:spawnVehicle",
-    "esx_status:set",
-    "HCheat:TempDisableDetection",
-    "UnJP"
-}
