e15571976379cd29b8fd67dbbc295dd400fb9a12
Something-Debug <peyton.bowe72@gmail.com>
2020-10-17 23:09:07 -0500

    DamageType and DamageModifier check can be toggled

--- client/cl_config.lua
+++ client/cl_config.lua
@@ -1,7 +1,10 @@
 Config = {}
 
-Config.SpectatorCheck = false
+Config.SpectatorCheck = true
 Config.MaxSpectatorStrikes = 5
-Config.GodModeCheck = false
+Config.GodModeCheck = true
 Config.MaxGodModeStrikes = 5
-Config.BlockedDamageTypes = { 4, 5, 8, 11}
+Config.DamageTypeCheck = true
+-- Damage types https://runtime.fivem.net/doc/natives/?_0x3BE0BB12D25FB305
+Config.BlockedDamageTypes = {}
+Config.DamageModifierCheck = true
