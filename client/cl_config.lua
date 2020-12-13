923dcb00457c10f09e35c4918730d66009af11fc
Something-Debug <peyton.bowe72@gmail.com>
2020-12-12 20:09:42 -0600

    Removed damge modifier check. Added new check.

--- client/cl_config.lua
+++ client/cl_config.lua
@@ -1,10 +1,17 @@
 Config = {}
-
-Config.SpectatorCheck = true
+-- Enable spectator checks
+Config.SpectatorCheck = false
+-- The max distance in meters a users camera can be away from them.
+Config.MaxCamCoords = 200
+-- The maximum strikes a user can receive for spectating someone 
 Config.MaxSpectatorStrikes = 5
-Config.GodModeCheck = true
+-- Enable GodMode checks
+Config.GodModeCheck = false
+-- Time in milliseconds to wait before performing checks
+Config.GodModeThreadDelay = 2000
+-- The maximum strikes a user can receive for being in godmode
 Config.MaxGodModeStrikes = 5
-Config.DamageTypeCheck = true
--- Damage types https://runtime.fivem.net/doc/natives/?_0x3BE0BB12D25FB305
-Config.BlockedDamageTypes = {}
-Config.DamageModifierCheck = true
+-- Enable vehicle speed modifier checks
+Config.SpeedModifierCheck = false
+-- The maximum allowed vehicle speed modifier. (Can not be less then 2)
+Config.MaximumSpeedModifier = 2
