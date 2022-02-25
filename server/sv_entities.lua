96036f90393c42b7990fb9a6653039170e812a95
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2022-02-25 08:52:13 -0600

    fix(sv_entities): disable check if lockdown is enabled

--- server/sv_entities.lua
+++ server/sv_entities.lua
@@ -1,22 +1,36 @@
+local isActive
 local allowedEntities = {}
+
 AddEventHandler('entityCreating', function(entity)
-  if not allowedEntities[tonumber(GetEntityModel(entity))] then
+  if (not allowedEntities[tonumber(GetEntityModel(entity))] and isActive) then
     CancelEvent()
   end
 end)
 
-AddEventHandler('vac_initalize_server', function(module)
-  if module == 'entities' or 'all' then
+AddEventHandler('__vac_internel:intalizeServer', function(module)
+  if (module == 'entities' or 'all') then
 
-    for hash in pairs(allowedEntities) do
-      allowedEntities[hash] = nil
+    if (GetConvar('sv_entityLockdown', 'inactive') == 'inactive') then
+      logger.warn('Client side spawning of entities is discouraged learn more here')
+
+      local next = next
+      if (next(allowedEntities) ~= nil) then
+        for hash in pairs(allowedEntities) do
+          allowedEntities[hash] = nil
+        end
+      end
+
+      local allowedModels = json.decode(GetConvar('valkyrie_allowed_entities', '[]'))
+
+      if (allowedModels ~= '') then
+        for i = 1, #models do
+          local hash = tonumber(GetHashKey(models[i]))
+
+          allowedEntities[hash] = true
+        end
     end
-
-    local models = json.decode(GetConvar('valkyrie_allowed_entities', '[]'))
-    for i = 1, #models do
-      local hash = tonumber(GetHashKey(models[i]))
-
-      rawset(allowedEntities, hash, true)
-    end
+    isActive = true
   end
+  isActive = false
+  print('[LOG] Entity lockdown enabled')
 end)
