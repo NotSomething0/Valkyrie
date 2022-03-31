77601eda2c9368f8be5f7a03bbc972fc54b9bf6f
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2022-03-31 12:55:18 -0500

    fix(sv_explosion): fix event name

--- server/sv_explosion.lua
+++ server/sv_explosion.lua
@@ -1,3 +1,18 @@
+-- Copyright (C) 2019 - 2022  NotSomething
+
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 local format = string.format
 local explosionLimit = 5
 local explosionIndex = {}
@@ -21,7 +36,7 @@ AddEventHandler('explosionEvent', function(sender, ev)
   end
 end)
 
-AddEventHandler('vac_initalize_server', function(module)
+AddEventHandler('__vac_internel:intalizeServer', function(module)
   if module == 'explosion' or 'all' then
 
     for index in pairs(explosionIndex) do
