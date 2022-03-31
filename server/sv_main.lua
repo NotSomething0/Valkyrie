bd487a91bd60a261963524aafdd27a8e934a8b2f
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2022-03-31 12:56:10 -0500

    fix(sv_main): trigger the correct event

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -1,3 +1,18 @@
+-- Copyright (C) 2019 - 2022  NotSomething
+
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 -- Micro optimizations https://www.lua.org/gems/sample.pdf
 local GetPlayers = GetPlayers
 local GetPlayerPed = GetPlayerPed
@@ -67,7 +82,7 @@ AddEventHandler('onResourceStart', function(resourceName)
     end
   end
 
-  TriggerEvent('vac_initalize_server', 'all')
+  TriggerEvent('__vac_internel:intalizeServer', 'all')
 end)
 
 AddEventHandler('playerDropped', function()
