1113d5009cece58d15c2432d29f34711d5d67502
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2022-09-26 18:22:41 -0500

    tweak(main): remove old logic
    Strike tracking is now handled by sv_internal along with this old code which included passing the client permissions has been removed

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -12,79 +12,60 @@
 
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
--- Micro optimizations https://www.lua.org/gems/sample.pdf
-local GetPlayers = GetPlayers
-local GetPlayerPed = GetPlayerPed
-local GetEntityMaxHealth = GetEntityMaxHealth
-local GetPlayerInvincible = GetPlayerInvincible
-local IsPlayerAceAllowed = IsPlayerAceAllowed
-
-local tracker = {}
-local function setUpPlayer(playerId)
-  local netId = tonumber(playerId)
-  if netId then
-    tracker[netId] = {strikes = 0, allowed = false}
-
-    if IsPlayerAceAllowed(netId, 'vac.bypass') then
-      tracker[netId].allowed = true
-      TriggerClientEvent('vac_receive_permission', netId, true)
-    else
-      TriggerClientEvent('vac_receive_permission', netId, false)
-    end
-  end
-end
-
-RegisterNetEvent('vac_player_activated', function()
-  setUpPlayer(source)
-end)
-
-CreateThread(function()
-  while true do
-    if next(GetPlayers()) ~= nil then
-      for _, netId in pairs(GetPlayers()) do
-        local source = tonumber(netId)
-        if tracker[source] then
-          local playerPed = GetPlayerPed(source)
-          local allowed = tracker[source].allowed
-
-          if GetPlayerInvincible(netId) and not allowed then
-            tracker[netId].strikes = tracker[netId].strikes + 1
-          end
-
-          if GetEntityMaxHealth(playerPed) >= 201 then
-            tracker[netId].strikes = tracker[netId].strikes + 1
-          end
-
-          if tracker[source].strikes >= 5 then
-            exports.Valkyrie:handlePlayer(netId, 'Maxium strikes', 'Exceeded maximum tracker strikes', true)
-          end
-        end
-        Wait(750)
-      end
-    else
-      Wait(5000)
-    end
-    Wait(0)
-  end
-end)
+local RESOURCE_NAME <const> = GetCurrentResourceName()
+local CURRENT_VERSION <const> = GetResourceMetadata(RESOURCE_NAME, 'version')
+local LATEST_VERSION = '0.0.0'
 
 AddEventHandler('onResourceStart', function(resourceName)
-  if (GetCurrentResourceName() ~= resourceName) then
+  if (RESOURCE_NAME ~= resourceName) then
     return
   end
 
-  local players = GetPlayers()
-
-  if next(players) then
-    for i = 1, #players do
-      setUpPlayer(tonumber(players[i]))
+  PerformHttpRequest('https://api.github.com/repos/NotSomething0/Valkyrie/releases', function(code, data)
+    if (code == 200) then
+      LATEST_VERSION = json.decode(data)[1].name
     end
-  end
 
-  TriggerEvent('__vac_internel:intalizeServer', 'all')
+    if (LATEST_VERSION ~= CURRENT_VERSION) then
+      print(('This version of Valkyrie is outdated! Please update as soon as possible!\n Latest Version: %s | Current Version: %s^7'):format(LATEST_VERSION, CURRENT_VERSION))
+    end
+  end)
+
+  TriggerEvent('__vac_internel:initialize', 'all')
 end)
 
-AddEventHandler('playerDropped', function()
-  tracker[source] = nil
+local godmodeCheck = false
+local healthCheck = false
+
+CreateThread(function()
+  while true do
+    Wait(1000)
+
+    for _, netId in pairs(GetPlayers()) do
+      local player = VPlayer(netId)
+
+      if (not IsPlayerAceAllowed(netId, 'vac:admin') and not IsPlayerAceAllowed(netId, 'vac:godmode')) then
+        local playerPed = GetPlayerPed(netId)
+
+        if (godmodeCheck and GetPlayerInvincible(netId)) then
+          player.strikes += 1
+        end
+
+        if (healthCheck and GetEntityMaxHealth(playerPed) >= 201) then
+          player.strikes += 1
+        end
+      end
+    end
+  end
+end)
+
+AddEventHandler('__vac_internel:initialize', function(module)
+  if (module ~= 'all' and module ~= 'main') then
+    return
+  end
+
+  godmodeCheck = GetConvarBool('vac:main:godmodeCheck', false)
+  healthCheck = GetConvarBool('vac:main:healthCheck', false)
+
+  log('info', ('[MAIN]: Updating basic checks Godmode Check: %s | Health Check: %s'):format(godmodeCheck, healthCheck))
 end)
