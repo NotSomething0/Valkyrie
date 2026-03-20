e0edd20cac5868c43586d239451877c6ed2c31be
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2026-03-19 20:09:33 -0500

    refactor(server/sv_util): remove GetConvarBool and GetPlayerIdentifiers and move UUID into the updated banManager class

--- server/sv_util.lua
+++ server/sv_util.lua
@@ -1,4 +1,4 @@
--- Copyright (C) 2019 - 2023  NotSomething
+-- Copyright (C) 2019 - 2026  NotSomething
 
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -13,87 +13,35 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-local _GetPlayerIdentifiers <const> = GetPlayerIdentifiers
+---Gets the name for the sender of a command "console" if playerIndex is zero
+---@param playerIndex string
+---@return string senderName
+function GetSenderName(playerIndex)
+  -- Supress IntelliSense wanting a string
+  playerIndex = tostring(playerIndex)
 
--- Overridden GetPlayerIdentifiers implementation exlcuding IP's and including tokens
--- @param netId | string | player source
--- @return table | all player identifiers and tokens
-function GetPlayerIdentifiers(netId)
-  if netId < 1 or not GetPlayerEndpoint(netId) then
-    return {}
+  if playerIndex == '0' then
+    return 'console'
   end
 
-  -- FxDK does not support identifier functions
-  if GetConvarInt('sv_fxdkMode', 0) == 1 then
-    return {['license'] = 'h8xor'}
+  if not DoesPlayerExist(playerIndex) then
+    error(('Invalid argument at index #1 player %s does not exist.'):format(playerIndex))
   end
 
-  local retval = {}
-  local playerIdentifiers = _GetPlayerIdentifiers(netId)
-  local playerTokens = GetPlayerTokens(netId)
-  local suffix, value
-
-  for i = 1, #playerIdentifiers do
-    suffix, value = playerIdentifiers[i]:match("^([^:]+):(.+)$")
-
-    -- Unrelible we don't check for VPN connections
-    if suffix ~= 'ip' then
-      retval[suffix] = value
-    end
-  end
-
-  for i = 1, #playerTokens do
-    suffix, value = playerTokens[i]:match("^([^:]+):(.+)$")
-
-    -- These can be unrelible so we'll ignore them
-    if suffix ~= '0' and suffix ~= '1' then
-      retval[suffix] = value
-    end
-  end
-
-  return retval
+  return GetPlayerName(playerIndex)
 end
 
--- https://github.com/jaymo1011/hackban/blob/master/convars.lua
--- Utility function instead of doing ternary statements  
--- @param varName | string | ConVar key to retrive data from
--- @param default | bool | Default value to return if the ConVar isn't specified
--- @return | bool | Value of the ConVar
-function GetConvarBool(varName, default)
-  local value = GetConvar(varName, '__nil__')
-
-  if value == 'true' then
-    return true
-  end
-
-  if value == 'false' then
-    return false
-  end
-
-  return default
-end
-
--- Utility function instead of checking if the message needs to be sent to the console or client
--- @param target | string | whom to send the message to
--- @param message | string | message content  
+---Utility function instead of checking if the message needs to be sent to the console or client
+---@param target string who the message should be sent to
+---@param message string
 function AddMessage(target, message)
-  if target == '0' then
-    print(message)
+  if target == 0 or target == '0' then
+    Citizen.Trace(message..'\n')
     return
   end
 
-  TriggerClientEvent('chat:addMessage', target, {
-    color = {255, 0, 0},
+  exports.chat:addMessage(target, {
+    color = {89, 0, 152},
     args = {'Valkyrie', message}
   })
-end
-
--- https://gist.github.com/jrus/3197011
--- Generate and return a pseudorandom UUID
--- @return string | UUID
-function UUID()
-  return string.gsub('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx', '[xy]', function(c)
-    local v = (c == 'x') and math.random(0, 0xf) or math.random(8, 0xb)
-    return string.format('%x', v)
-  end)
 end
